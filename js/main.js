66823a1d3709bbc41f36ba17f7b13abf08586dbf
Davydov Roman <rdfrontend1@gmail.com>
2025-10-22 01:23:05 +0300

    day 3, block 3 + slider, commit & push

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 new WOW().init();
+var mySwiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: true
+    }
+})
